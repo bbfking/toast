b9da9f29e1bd0a0654aab2122f050496acccaa97
pfXiong <bbfking@163.com>
2014-05-03 21:01:45 +0800

    Toast项目

---
+++ src/org/equinoxosgi/toast/Gps.java
@@ -0,0 +1,16 @@
+package org.equinoxosgi.toast;
+
+public class Gps {
+	public int getHeading(){
+		return 90;
+	}
+	public int getLatitude(){
+		return 3776999;
+	}
+	public int getLongitude(){
+		return -12244694;
+	}
+	public int getSpeed(){
+		return 50;
+	}
+}
